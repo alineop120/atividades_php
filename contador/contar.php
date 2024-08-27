5188d57bd9dd3d4737cf74c3e461132cf0103eb8
Aline Pinho <134012132+alineop120@users.noreply.github.com>
2024-08-27 16:12:02 -0300

    atualizando a pasta contador

--- contador/contar.php
+++ contador/contar.php
@@ -6,26 +6,25 @@
 	<h1> CONTADOR </h1>
 	<h2> Usando o WHILE </h2>
 	<?php
+	$cont = $_GET["contar"];
 		//Valor Inicializado
 		$cont = 1;
 		while ($cont <= 10) {
 			echo "<p> Estou contando $cont vezes</p>";
 			$cont = $cont + 1;
-		}	
-    ?>
-	<h2> Usando o DO..WHILE </h2>
-	<?php
-		$cont = 1;
-		Do {
-			echo "<p> Estou contando $cont vezes </p>";
-			$cont += 1;
-		}while($cont <= 10);
-	?>
-	<h2> Usando o FOR </h2>
-	<?php
-		for ($cont = 1; $cont <= 10; $cont++) {
-			echo "<p> Estou contando $cont vezes </p>";
 		}
-	?>
+	
+	echo "<h2> Usando o DO..WHILE </h2>";
+	$cont = 1;
+	Do {
+		echo "<p> Estou contando $cont vezes </p>";
+		$cont += 1;
+	}while($cont <= 10);
+	
+	echo "<h2> Usando o FOR </h2>";
+	for ($cont = 1; $cont <= 10; $cont++) {
+		echo "<p> Estou contando $cont vezes </p>";
+	}
+	?>	
 </body>
 </html>
---
+++ contador/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	
+</head>
+<body>
+	<h1> CONTADOR </h1>
+	<form action="contar.php" method="get">
+		<p>
+			Contar até 
+			<input type="text" name="contar">
+		</p>
+		<p>
+			<input type="submit" value="Contar">
+		</p>
+	</form>
+</body>
+</html>
